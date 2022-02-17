0e55a6c079514202010e73bfc667ddf11b8232d2
yakovbenalex <yakovbenalex@mail.ru>
2022-02-18 00:09:53 +0300

    Lesson 1.3: Text and scrolling views

---
+++ unit1_3textandscrollingviews/src/main/java/ru/opalevapps/unit1_3textandscrollingviews/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.opalevapps.unit1_3textandscrollingviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
